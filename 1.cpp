1ff9d178281fc4c148027d9c15bccbfa16a362b9
NazarKiriak <57nazar57@gmail.com>
2021-10-28 21:47:23 +0300

    commit 1

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,7 @@ int main()
 {
 using namespace std;
     int x;
-    cin >> x;
+    cin >> x; //first add
     int a = x / 100, b = (x / 10) % 10, c = x % 10;
     int ans = a * 100 + c * 10 + b;
     cout <<ans;
